d563d1aabeb31127dc2a4ed9d09e9fd8ee1c568a
Harry Chen <harrychenca@gmail.com>
2015-01-06 07:41:13 +0800

    Storing profile pic in S3

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,15 @@ class User < ActiveRecord::Base
   has_many :post_votes
 
   # uploading avatar
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar,
+                    :storage => :s3,
+                    :s3_credentials => Proc.new{|a| a.instance.s3_credentials },
+                    :styles => { :medium => "300x300>", :thumb => "100x100>" },
+                    :default_url => "/images/:style/missing.png"
+
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+  def s3_credentials
+    {:bucket => ENV['S3_BUCKET'], :access_key_id => ENV['ACCESS_KEY_ID'], :secret_access_key => ENV['SECRET_ACCESS_KEY']}
+  end
 end
